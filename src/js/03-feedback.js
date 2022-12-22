a1c1e1293cda70ca8a4b23b7b0567bfa1dba7ff6
HrechykhaYurii <grechikha.yura@gmail.com>
2022-12-22 20:08:11 +0200

    add task3

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,41 @@
+import throttle from 'lodash.throttle';
 
+
+const form = document.querySelector('.feedback-form');
+const input = form.querySelector('input');
+const textarea = form.querySelector('textarea');
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onInput, 500));
+
+
+const LOCALSTORAGE_KEY = 'feedback-form-state';
+
+populateForm();
+
+function onInput() {
+  const formData = {
+    email: form.email.value,
+    message: form.message.value,
+  };
+  localStorage.setItem(LOCALSTORAGE_KEY, JSON.stringify(formData));
+}
+
+function onFormSubmit(evt) {
+  evt.preventDefault();
+
+  const formDataObject = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) || {};
+  console.log(formDataObject);
+
+  evt.target.reset();
+  localStorage.removeItem(LOCALSTORAGE_KEY);
+}
+
+function populateForm() {
+  if (localStorage.getItem(LOCALSTORAGE_KEY)) {
+    const formDataObject = JSON.parse(localStorage.getItem(LOCALSTORAGE_KEY)) || {};
+
+    textarea.value = formDataObject.message || '';
+    input.value = formDataObject.email || '';
+  }
+}
